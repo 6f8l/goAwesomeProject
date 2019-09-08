7a31399b42774226b573a5e4d7ed72553e0c7694
6f8l <atsuki02jojo@gmail.com>
2019-09-08 17:47:04 +0900

    Test sqlite

--- main.go
+++ main.go
@@ -1,29 +1,90 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 
-	"gopkg.in/ini.v1"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type ConfigList struct {
-	Port      int
-	DbName    string
-	SQLDriver string
+type Person struct {
+	Name string
+	Age  int
 }
 
-var Config ConfigList
+var DbConnection *sql.DB
 
-func init() {
-	cfg, _ := ini.Load("config.ini")
-	Config = ConfigList{
-		Port:      cfg.Section("web").Key("port").MustInt(),
-		DbName:    cfg.Section("db").Key("name").MustString("example.sql"),
-		SQLDriver: cfg.Section("db").Key("driver").String(),
-	}
-}
 func main() {
-	fmt.Printf("%T, %v\n", Config.Port, Config.Port)
-	fmt.Printf("%T, %v\n", Config.DbName, Config.DbName)
-	fmt.Printf("%T, %v\n", Config.SQLDriver, Config.SQLDriver)
+	DbConnection, _ := sql.Open("sqlite3", "./example.sql")
+	defer DbConnection.Close()
+	cmd := `create table if not exists person(
+				name string,
+				age int
+			)`
+	_, err := DbConnection.Exec(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// cmd = "insert into person (name, age) values (?, ?)"
+	// // cmd = "update person set age = ? where name = ?"
+	// _, err = DbConnection.Exec(cmd, "Nancy", 20)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// cmd = "select * from person"
+	// rows, _ := DbConnection.Query(cmd)
+	// defer rows.Close()
+	// var pp []Person
+	// for rows.Next() {
+	// 	var p Person
+	// 	err := rows.Scan(&p.Name, &p.Age)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	pp = append(pp, p)
+	// }
+
+	// for _, p := range pp {
+	// 	fmt.Println(p.Name, p.Age)
+	// }
+
+	// cmd = "select * from person where age = ?"
+	// row := DbConnection.QueryRow(cmd, 1000)
+	// var p Person
+	// err = row.Scan(&p.Name, &p.Age)
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		log.Println("No row")
+	// 	} else {
+	// 		log.Println(err)
+	// 	}
+	// }
+	// fmt.Println(p.Name, p.Age)
+
+	// cmd = "delete from person where name = ?"
+	// _, err = DbConnection.Exec(cmd, "Nancy")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	tableName := "person"
+	cmd = fmt.Sprintf("select * from %s", tableName)
+	rows, _ := DbConnection.Query(cmd)
+	defer rows.Close()
+	var pp []Person
+	for rows.Next() {
+		var p Person
+		err := rows.Scan(&p.Name, &p.Age)
+		if err != nil {
+			log.Println(err)
+		}
+		pp = append(pp, p)
+	}
+
+	for _, p := range pp {
+		fmt.Println(p.Name, p.Age)
+	}
 }
